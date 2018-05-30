42bbc48dc8a9359ead892cf4a07f601c0c50e655
Stefan Kaiser <stefan.kaiser@htl.rennweg.at>
2018-05-30 14:12:08 +0200

    asdf

--- ue16/app/components/einkaufen.spec.js
+++ ue16/app/components/einkaufen.spec.js
@@ -13,6 +13,7 @@ describe("einkaufenController", function () {
         einkaufenController = controller("einkaufenController", {
         });
     });
+    /*
     beforeEach(inject(function($controller) {
         var ShopService = {
             warenkorb: [],
@@ -26,6 +27,7 @@ describe("einkaufenController", function () {
         spyOn(ShopService, 'add', 'del', 'replaze').and.returnValue("ShopService");
 
     }));
+    */
     it("Instanz wird erzeugt", function () {
         expect(einkaufenController).toBeDefined();
     });
@@ -42,6 +44,29 @@ describe("einkaufenController", function () {
             einkaufenController.formular = {$setUntouched : function () {
             }};
             einkaufenController.neuArtikel();
-            expect(ShopService.warenkorb.length).toEqual(1);
+            expect(einkaufenController.warenkorb.length).toEqual(1);
+        expect(einkaufenController.artikelname).toEqual("");
+        expect(einkaufenController.anzahl).toEqual("");
+        einkaufenController.artikelname = "Test2";
+        einkaufenController.anzahl = 49;
+        einkaufenController.formular = {$setUntouched : function () {
+        }};
+        einkaufenController.neuArtikel();
+        expect(einkaufenController.warenkorb.length).toEqual(2);
         });
+    it("funktionen testen2", () => {
+        einkaufenController.anzahl = 49;
+        einkaufenController.artikelname = "Test";
+        einkaufenController.formular = {$setUntouched : function () {
+        }};
+        einkaufenController.neuArtikel();
+        einkaufenController.anzahl = 449;
+        einkaufenController.artikelname = "Test2";
+        einkaufenController.formular = {$setUntouched : function () {
+        }};
+        einkaufenController.neuArtikel();
+        expect(einkaufenController.warenkorb.length).toEqual(2);
+        einkaufenController.loesch();
+        expect(einkaufenController.warenkorb.length).toEqual(1);
+    });
 });
